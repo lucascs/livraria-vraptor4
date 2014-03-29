618c45ea4b25c7c14991ed5e277eb214d94a783a
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2014-03-29 10:56:36 -0300

    Usando @PersistenceContext para pegar o EntityManager

--- livraria-admin/src/main/java/br/com/casadocodigo/livraria/persistencia/FabricaDaJPA.java
+++ livraria-admin/src/main/java/br/com/casadocodigo/livraria/persistencia/FabricaDaJPA.java
@@ -1,29 +1,15 @@
 package br.com.casadocodigo.livraria.persistencia;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.slf4j.Logger;
-
 public class FabricaDaJPA {
-
 	@PersistenceContext
 	private EntityManager manager;
-	
-	@Inject
-	private Logger logger;
-	
-	@PostConstruct
-	public void inicia() {
-		logger.info("Hooooooooo");
-	}
-	
+
 	@Produces
-	public EntityManager getEntityManager() {
-		logger.info("Haaaa");
+	public EntityManager getManager() {
 		return manager;
-	}
+	}	
 }
